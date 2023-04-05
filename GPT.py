59a3125fd30a2b839ae33b62152a6b06aadf2e2d
Kylar Osborne <kylar@Kylars-MacBook-Air.local>
2023-04-05 17:32:14 +1200

    It kinda worksgit status

--- GPT.py
+++ GPT.py
@@ -1,74 +1,164 @@
 import openai
 import uuid
-
 import API_KEY
 
 openai.api_key = API_KEY.API_KEY
 
+
 class GPTNode:
-    def __init__(self, task=None, parent=None, role="executor"):
-        self.id = uuid.uuid4()
+
+    id_counter = 0
+    def __init__(self, task=None, parent=None):
+        self.id = GPTNode.id_counter
+        GPTNode.id_counter += 1
         self.parent = parent
         self.children = []
         self.task = task
-        self.role = role
         self.state = "idle"
 
-    def decompose_task(self):
-        prompt = f"Decompose the following task into smaller subtasks: '{self.task}'"
-        response = openai.Completion.create(
-            engine="text-davinci-002",
-            prompt=prompt,
-            max_tokens=100,
-            n=1,
-            stop=None,
-            temperature=0.5,
+
+        # prompt = f"Do not include file extension. Only use lowercase letters and underscores. Write a filename that is relevant to the following task: {task}"
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": "You a file namer. You do not include file extensions. You always format your replies as firstword_secondword"},
+                {"role": "user",
+                 "content": f"Write a file name to describe the following task.\n'{self.task}'"},
+            ],
         )
-        return response.choices[0].text.strip().split("\n")
+
+        self.task_short = response['choices'][0]['message']['content'].strip()
+        print(f"{self}: I am born to do {self.task_short}")
+        with open(f"{self.__str__()}_task_{self.task_short}.txt", "w") as f:
+            f.write(self.task)
+
+    def can_solve_task(self):
+        # prompt = f"Answer only yes or no. Can the following task fully and completely be solved using a Python script: '{self.task}'?"
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": "You are a helper that will answer only either yes or no."},
+                {"role": "user",
+                 "content": f"Can the following task fully and completely be solved using a Python script: '{self.task}'?"},
+            ],
+        )
+        answer = response['choices'][0]['message']['content'].strip().lower()
+        print(f"{self}: Solving {self.task_short} via Python={answer} for {self.task[0:20]}")
+        return "yes" in answer
+
+    def decompose_task(self):
+        # prompt = f"When writing subtasks, include complete context within each subtask. Fully and completely decompose the following task into all necessary subtasks: '{self.task}'"
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": "You are a task decomposer. You include full context alongside each subtask description."},
+                {"role": "user",
+                 "content": f"Fully and completely decompose the following task into all necessary subtasks: '{self.task}'"},
+            ],
+        )
+        print(f"{self}: decomposed task" )
+        return response['choices'][0]['message']['content'].strip().split("\n")
 
     def execute_task(self):
-        prompt = f"Write Python code to accomplish the following task: '{self.task}'"
-        response = openai.Completion.create(
-            engine="text-davinci-002",
-            prompt=prompt,
-            max_tokens=100,
-            n=1,
-            stop=None,
-            temperature=0.5,
+        # prompt = f"Only respond with Python code. Write a full, complete, and executable Python script to accomplish the following task: '{self.task}'"
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": "You are a Python code generator. You can only write Python."},
+                {"role": "user",
+                 "content": f"Write a full, complete, and executable Python script to accomplish the following task: '{self.task}'"},
+            ],
         )
-        return response.choices[0].text.strip()
+
+        code = response['choices'][0]['message']['content'].strip()
+        # prompt = f"Do not include file extension. Only use lowercase letters and underscores. Write a file name that is relevant to the following code:\n{code}"
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system",
+                 "content": "You a file namer. You do not include file extensions. You always format your replies as firstword_secondword"},
+                {"role": "user",
+                 "content": f"Write a file name to describe the following code.\n'{code}'"},
+            ],
+        )
+
+        print(f"{self}: wrote a python script")
+
+        file_name = response['choices'][0]['message']['content'].strip()
+        with open(f"{self.__str__()}_code_{file_name}.py", "w") as f:
+            f.write(code)
+        return code
 
     def verify_task(self, code):
-        prompt = f"Verify if the following Python code accomplishes the task '{self.task}':\n{code}"
-        response = openai.Completion.create(
-            engine="text-davinci-002",
-            prompt=prompt,
-            max_tokens=50,
-            n=1,
-            stop=None,
-            temperature=0.5,
+        # prompt = f"Answer only yes or no. Verify if the following Python code accomplishes the task. '{self.task}':\n{code}"
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": "You are a code verifier. You always respond with either yes or no."},
+                {"role": "user",
+                 "content": f"Verify if the following Python code accomplishes the task. '{self.task}':\n{code}"},
+            ],
         )
-        return response.choices[0].text.strip().lower() == "yes"
+        return response['choices'][0]['message']['content'].strip().lower() == "yes"
 
-    def create_child(self, task, role):
-        child = GPTNode(task=task, parent=self, role=role)
-        self.children.append(child)
-        return child
+    def generate_debug_prompt(self, code):
+        # prompt = f"Generate a debug prompt for the following Python code that failed to accomplish the task: '{self.task}'\n code:\n{code}"
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": "You are a debug prompt generator."},
+                {"role": "user",
+                 "content": f"Generate a debug prompt for the following Python code that failed to accomplish the task: '{self.task}'\n code:\n{code}"},
+            ],
+        )
+        print(f"{self}: verified code??")
+        return response['choices'][0]['message']['content'].strip()
 
     def process(self):
-        if self.role == "executor":
-            if self.state == "idle":
-                self.state = "executing"
+        if self.state == "idle":
+            if self.can_solve_task():
+                self.state = "executor"
                 code = self.execute_task()
-                self.state = "completed"
-                return code
+                self.state = "verifier"
+                task_completed = self.verify_task(code)
+                if task_completed:
+                    self.state = "done"
+                else:
+                    debug_prompt = self.generate_debug_prompt(code)
+                    debug_node = GPTNode(task=debug_prompt, parent=self)
+                    self.children.append(debug_node)
             else:
-                return None
-        elif self.role == "verifier":
-            if self.state == "idle" and self.parent.state == "completed":
-                self.state = "verifying"
-                task_completed = self.verify_task(self.parent.result)
-                self.state = "verified"
-                return task_completed
+                self.state = "decomposer"
+                subtasks = self.decompose_task()
+                for subtask in subtasks:
+                    child = GPTNode(task=subtask, parent=self)
+                    self.children.append(child)
+        elif self.state == "decomposer" and all(child.state == "done" for child in self.children):
+            self.state = "verifier"
+            # Additional logic for spawning verifier nodes and
+            # verification can be implemented here.
+            verifier_node = GPTNode(task=f"Verify '{self.task}'", parent=self)
+            self.children.append(verifier_node)
+            task_completed = verifier_node.process()
+            if task_completed:
+                self.state = "done"
             else:
-                return None
+                # Handle task failure or create debugging tasks here
+                print("Task not completed xxx")
+                # In case of failure, reset the node's state to "idle" for reprocessing.
+                self.state = "idle"
+
+    def process_all_children(self):
+        for child in self.children:
+            child.process()
+            if child.state not in ["done", "verifier"]:
+                child.process_all_children()
+
+    def __str__(self):
+        if self.parent is not None and isinstance(self.parent, GPTNode):
+            return str(self.parent.id) + "_" + str(self.id)
+        else:
+            return str(self.id)
+
+    def __repr__(self):
+        return self.__str__()
--- main.py
+++ main.py
@@ -2,36 +2,34 @@ from GPT import GPTNode
 
 def traverse_tree(node):
     results = {}
+    node.process()
+    if node.state == "executor":
+        result = node.execute_task()
+        results[node.id] = result
+        print(f"Executor {node.id} completed task: {node.task}")
+    elif node.state == "verifier":
+        task_completed = node.verify_task(node.parent.result)
+        if task_completed:
+            print(f"Verifier {node.id} verified task: {node.task}")
+            node.state = "done"
+        else:
+            print(f"Verifier {node.id} failed to verify task: {node.task}")
+            # Handle task failure or create debugging tasks here
+
     for child in node.children:
-        if child.role == "executor":
-            result = child.process()
-            if result is not None:
-                results[child.id] = result
-                print(f"Executor {child.id} completed task: {child.task}")
-
-        if child.role == "verifier":
-            task_completed = child.process()
-            if task_completed is not None:
-                print(f"Verifier {child.id} verified task: {child.parent.task}")
-            else:
-                print(f"Verifier {child.id} failed to verify task: {child.parent.task}")
-                # Handle task failure or create debugging tasks here
-
         child_results = traverse_tree(child)
         results.update(child_results)
 
     return results
 
 if __name__ == "__main__":
-    main_task = "Create a personal blog with photos of cats and written articles about creating AI and host it locally"
+    main_task = "Create an ascii art of a cat"
     master_node = GPTNode(task=main_task)
 
-    subtasks = master_node.decompose_task()
-    for subtask in subtasks:
-        executor = master_node.create_child(task=subtask, role="executor")
-        verifier = executor.create_child(task=f"Verify {subtask}", role="verifier")
+    results = {}
+    while master_node.state != "done":
+        results = traverse_tree(master_node)
 
-    results = traverse_tree(master_node)
     print("All tasks completed and verified:")
     for task_id, code in results.items():
         print(f"{task_id}: {code}")
